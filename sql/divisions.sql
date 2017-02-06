b0bccbb76b931cad83d552fe4d7aa5ea1819ad95
Noor <webmaster.noor@gmail.com>
2017-02-06 09:50:49 +0600

    updated

--- sql/divisions.sql
+++ sql/divisions.sql
@@ -1,17 +1,23 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.7
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: May 12, 2014 at 09:56 AM
--- Server version: 5.5.29
--- PHP Version: 5.4.10
+-- Host: 127.0.0.1
+-- Generation Time: Jan 11, 2017 at 06:05 AM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 5.6.24
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
--- Database: `bd_geocode`
+-- Database: `timf`
 --
 
 -- --------------------------------------------------------
@@ -21,21 +27,42 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `divisions` (
-  `id` int(2) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) NOT NULL,
-  `bn_name` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+  `id` int(11) NOT NULL,
+  `name` varchar(500) DEFAULT NULL,
+  `DivisionName` varchar(500) DEFAULT NULL,
+  `DivisionNameBangla` varchar(500) DEFAULT NULL,
+  `unitprice` decimal(10,2) DEFAULT '0.00',
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `divisions`
 --
 
-INSERT INTO `divisions` (`id`, `name`, `bn_name`) VALUES
-(1, 'Barisal', 'বরিশাল'),
-(2, 'Chittagong', 'চট্টগ্রাম'),
-(3, 'Dhaka', 'ঢাকা'),
-(4, 'Khulna', 'খুলনা'),
-(5, 'Rajshahi', 'রাজশাহী'),
-(6, 'Rangpur', 'রংপুর'),
-(7, 'Sylhet', 'সিলেট');
+INSERT INTO `divisions` (`id`, `name`, `DivisionName`, `DivisionNameBangla`, `unitprice`, `created_at`, `updated_at`) VALUES
+(2, NULL, 'Rajshahi', 'রাজশাহী', '0.00', '2017-01-10 11:09:31', '2017-01-10 11:09:31');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `divisions`
+--
+ALTER TABLE `divisions`
+  ADD PRIMARY KEY (`id`);
+ALTER TABLE `divisions` ADD FULLTEXT KEY `idx_name` (`name`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `divisions`
+--
+ALTER TABLE `divisions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
