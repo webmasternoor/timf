59e868c8b8f5765eba91ff18bcb8c77f89601834
Rockey Ahmed <rockeycse@gmail.com>
2017-03-18 10:21:52 +0600

    saving schedule
    saving schedule

--- resources/views/saving1/create1.blade.php
+++ resources/views/saving1/create1.blade.php
@@ -6,37 +6,47 @@
         <div class="col-md-12">
             {!! Form::select("SamityId",$SamityId,null,["class"=>"form-control SamityId required","id"=>"SamityId"]) !!}
             {{--<select name="SamityId" id="SamityId">--}}
-                {{--<option value="" selected>--select--</option>--}}
-                {{--@foreach($SamityId as $zone_data )--}}
-                    {{--<option value="{{$zone_data->id}}"--}}
-                            {{-->{{$zone_data->SomitiName}}</option>--}}
-                {{--@endforeach--}}
+            {{--<option value="" selected>--select--</option>--}}
+            {{--@foreach($SamityId as $zone_data )--}}
+            {{--<option value="{{$zone_data->id}}"--}}
+            {{-->{{$zone_data->SomitiName}}</option>--}}
+            {{--@endforeach--}}
             {{--</select>--}}
             <span id="SamityId-error" class="help-block"></span>
         </div>
     </div>
 </div>
 
-<table class="dynatable" id="dynatable">
+<table class="table table-bordered table-striped" id="dynatable">
     <thead>
     <tr>
-        <th>Type</th>
-        <th>Account Name</th>
-        <th>Debit</th>
-        <th>Credit</th>
+        <th style="text-align: center">SL No</th>
+        <th >Member Name</th>
+        <th>Account No</th>
+        <th>Account Type</th>
+        <th>Amount</th>
+        {{--<th>Date</th>--}}
     </tr>
     </thead>
     <tbody id="p_scents" class="p_scents">
 
     </tbody>
 </table>
-
+<div class="form-group">
+    <div class="col-md-6 col-md-push-3">
+        <a href="javascript:ajaxLoad('saving1/list')" class="btn btn-danger"><i
+                    class="glyphicon glyphicon-backward"></i>
+            Back</a>
+        {!! Form::button("<i class='glyphicon glyphicon-floppy-disk'></i> Save",["type" => "submit","class"=>"btn
+    btn-primary"])!!}
+    </div>
+</div>
 {{--@if(!empty($data))--}}
-    {{--@foreach ($data as $data1):--}}
-    {{--@if($data1->MemberId)--}}
-        {{--{{$data1->MemberId}}--}}
-    {{--@endif--}}
-    {{--@endforeach--}}
+{{--@foreach ($data as $data1):--}}
+{{--@if($data1->MemberId)--}}
+{{--{{$data1->MemberId}}--}}
+{{--@endif--}}
+{{--@endforeach--}}
 {{--@endif--}}
 {{--@include("saving1._form1")--}}
 {!! Form::close() !!}
@@ -45,7 +55,7 @@
     $(document).ready(function () {
         $(document).on('change', '.SamityId', function () {
             var SamityId = $(this).val();
-            var i=0;
+            var i=1;
             $('#p_scents').empty();
             $.ajax({
                 type: 'get',
@@ -54,7 +64,7 @@
                 success: function (data) {
 //                    console.log(data);
                     $.each(data, function (index, subcatObj3p) {
-                        $('#p_scents').append('<tr><td><input type="text" name="debit_amount" id="'+subcatObj3p.MemberId+'" value="'+subcatObj3p.MemberId+'"/></td><td><input type="text" name="credit_amount" id="credit_amount"/></td></tr>');
+                        $('#p_scents').append('<tr><td style="text-align: center">'+i+'</td> <td><input type="text" name="MemberId'+i+'" id="'+subcatObj3p.MemberId+'" value="'+subcatObj3p.MemberId+'"/></td> <td><input type="text" name="AccountNo'+i+'" id="'+subcatObj3p.AccountNo+'" value="'+subcatObj3p.AccountNo+'"/></td> <td><input type="text" name="ProductId'+i+'" id="'+subcatObj3p.SavingType+'" value="'+subcatObj3p.SavingType+'"/></td> <td><input type="number" name="Amount'+i+'" id="Amount'+i+'" value=""/></td></tr>');
                         i++;
                     });
                 },
